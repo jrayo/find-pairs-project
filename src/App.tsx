fa8eededc68f932759c963d1f4479934b500b988
Juan Felipe <juan.rayo@globant.comm>
2022-08-23 20:28:24 -0500

    Simple UI that uses the findPair function

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import findPairs  from './utils';
 import './App.css';
 
 function App() {
+  const [listOfIntegers, setListOfIntegers] = useState<Array<number>>([])
+  const [target, setTarget] = useState<number>(0);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const inputNumber = Number((event.currentTarget.elements[0] as HTMLInputElement).value)
+      if(listOfIntegers.includes(inputNumber)){
+        return null;
+      }
+      event.currentTarget.reset();
+      setListOfIntegers([...listOfIntegers, inputNumber]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <form>
+          <label>
+          Current Target: 
+          </label>
+          <input name='list' type='number' placeholder="0" onChange={(event)=>{setTarget(Number(event.target.value))}}/>
+        </form>
+    
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="List">Add number to the list</label>
+          <input name='List' id="List" type='number' placeholder="0"/>
+          <button type='submit'> ADD </button>
+        </form>
+
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Current List: {JSON.stringify(listOfIntegers)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=>setListOfIntegers([])}> CLEAR LIST </button>
+
+        <p>
+          Results: {JSON.stringify(findPairs(listOfIntegers, target))}
+        </p>
+        
       </header>
     </div>
   );
